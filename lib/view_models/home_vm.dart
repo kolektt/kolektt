f62f1b0a04278b0addac20cd200eb9ee9925597f
brainer <brainer@kakao.com>
2025-02-24 13:55:54 +0900

    Add new music taste entry
    - Introduced a new `MusicTaste` entry with id 'mt2'
    - Added title "Take Five" and subtitle "Dave Brubeck"
    - Used existing image URL for the new entry
    - Linked to sample data record at index 1

--- lib/view_models/home_vm.dart
+++ lib/view_models/home_vm.dart
@@ -170,6 +170,13 @@ class MusicTaste {
       imageUrl:
       "https://s.pstatic.net/dthumb.phinf/?src=%22https%3A%2F%2Fs.pstatic.net%2Fshop.phinf%2F20250217_3%2F17397898415832ySrH_PNG%2FED9994EBA9B4%252BECBAA1ECB298%252B2025-02-17%252B195422.png%22&type=ff364_236&service=navermain",
       record: Record.sampleData[0],
+    ),
+    MusicTaste(
+      id: 'mt2',
+      title: "Take Five",
+      subtitle: "Dave Brubeck",
+      imageUrl: "https://s.pstatic.net/dthumb.phinf/?src=%22https%3A%2F%2Fs.pstatic.net%2Fshop.phinf%2F20250217_3%2F17397898415832ySrH_PNG%2FED9994EBA9B4%252BECBAA1ECB298%252B2025-02-17%252B195422.png%22&type=ff364_236&service=navermain",
+      record: Record.sampleData[1],
     )
   ];
 }
